59b9bfaa4646472050dd239b473fff61cac7ebd2
nabeel-travelwits <nabeel@travelwits.com>
2026-01-16 21:41:50 +0500

    v3.6

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(fileUpload());
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
